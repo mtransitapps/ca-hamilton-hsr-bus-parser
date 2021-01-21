aac081b2875b4ce49bdbde584b7dc9552b34de3a
Mathieu Méa <mathieu.mea@gmail.com>
2021-01-21 18:48:55 -0500

    GTFS Direction Finder > enable

--- src/main/java/org/mtransit/parser/ca_hamilton_hsr_bus/HamiltonHSRBusAgencyTools.java
+++ src/main/java/org/mtransit/parser/ca_hamilton_hsr_bus/HamiltonHSRBusAgencyTools.java
@@ -17,9 +17,7 @@ import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MRoute;
 import org.mtransit.parser.mt.data.MTrip;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
@@ -144,279 +142,37 @@ public class HamiltonHSRBusAgencyTools extends DefaultAgencyTools {
 		);
 	}
 
-	private static final String GO = "GO";
-	private static final String TRANSIT_TERMINAL_SHORT = "TT"; // Transit Terminal
-	private static final String EAST = "East";
-	private static final String GREENE_SHORT = "Grn";
-	private static final String HIGH_SCHOOL_SHORT = "HS"; // High School
-	private static final String HAMILTON_AIRPORT_SHORT = "Airport"; // Hamilton
-	private static final String HAMILTON_WATERFRONT_SHORT = "Waterfront"; // Hamilton
-	private static final String HERITAGE = "Heritage";
-	private static final String MAC_NAB = "MacNab";
-	private static final String WEST = "West";
-	private static final String BURLINGTON = "Burlington";
+	@Override
+	public boolean directionFinderEnabled() {
+		return true;
+	}
+
+	private static final Pattern STARTS_WITH_RSN_LETTER = Pattern.compile("(^[\\d]+[a-z] )", Pattern.CASE_INSENSITIVE);
+
+	@Nullable
+	@Override
+	public String selectDirectionHeadSign(@Nullable String headSign1, @Nullable String headSign2) {
+		if (StringUtils.equals(headSign1, headSign2)) {
+			return null; // can NOT select
+		}
+		final boolean startsWithLetter1 = headSign1 != null && STARTS_WITH_RSN_LETTER.matcher(headSign1).find();
+		final boolean startsWithLetter2 = headSign2 != null && STARTS_WITH_RSN_LETTER.matcher(headSign2).find();
+		if (startsWithLetter1) {
+			if (!startsWithLetter2) {
+				return headSign2;
+			}
+		} else if (startsWithLetter2) {
+			return headSign1;
+		}
+		return null;
+	}
 
 	@Override
 	public boolean mergeHeadsign(@NotNull MTrip mTrip, @NotNull MTrip mTripToMerge) {
-		List<String> headsignsValues = Arrays.asList(mTrip.getHeadsignValue(), mTripToMerge.getHeadsignValue());
-		if (mTrip.getRouteId() == 1L) {
-			if (Arrays.asList( //
-					"1A University Plz", //
-					"Hamilton " + GO + " Ctr" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Hamilton " + GO + " Ctr", mTrip.getHeadsignId());
-				return true;
-			} else if (Arrays.asList( //
-					"Downtown", //
-					"Eastgate Sq", //
-					"Fiesta Mall" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Fiesta Mall", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 5L) {
-			if (Arrays.asList( //
-					"Downtown", // <>
-					"5A Greenhill @ Cochrane", //
-					"5E Quigley @ Greenhill", //
-					"Jones @ King", //
-					"Main @ MacNab", //
-					EAST //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString(EAST, mTrip.getHeadsignId());
-				return true;
-			} else if (Arrays.asList( //
-					"Downtown", // <>
-					"Meadowlands", //
-					"52 Head St", //
-					"Head St", //
-					"52 Pirie @ Governors", //
-					"5C Meadowlands", //
-					"5C West Hamilton Loop", //
-					"West Hamilton Loop", //
-					WEST //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString(WEST, mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 10L) {
-			if (Arrays.asList( //
-					"Downtown", //
-					"Eastgate Sq" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Eastgate Sq", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 20L) {
-			if (Arrays.asList( //
-					"Mtn TC P&R", //
-					"Waterfront @ Pier 8" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Waterfront @ Pier 8", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 21L) {
-			if (Arrays.asList( //
-					"Fennell @ West 5th", //
-					"Mohawk Collegw", //
-					"Mohawk College", //
-					"MacNab TT" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("MacNab TT", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 23L) {
-			if (Arrays.asList( //
-					"Upper Gage @ Mohawk", //
-					"MacNab Terminal", //
-					"MacNab TT" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("MacNab TT", mTrip.getHeadsignId());
-				return true;
-			}
-			if (Arrays.asList( //
-					"Upper Gage @ Rymal", //
-					"Rymal @ Upper Gage" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Rymal @ Upper Gage", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 24L) {
-			if (Arrays.asList( //
-					"Upper Sherman @ Mohawk", //
-					"John @ Jackson", //
-					"MacNab TT" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("MacNab TT", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 26L) {
-			if (Arrays.asList( //
-					"Rymal @ Upper Wellington Only", //
-					"King @ John", //
-					"MacNab TT" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("MacNab TT", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 33L) {
-			if (Arrays.asList( //
-					"Fennell @ West 5th", //
-					"Mohawk Terminal", //
-					"MacNab TT" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("MacNab TT", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 34L) {
-			if (Arrays.asList( //
-					"34A Upper Horning Loop", //
-					"Glancaster & Upper Horning Loops", //
-					"Glancaster Loop" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Glancaster Loop", mTrip.getHeadsignId());
-				return true;
-			}
-			if (Arrays.asList( //
-					"Mohawk @ Upper Paradise", //
-					"Downtown" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Downtown", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 35L) {
-			if (Arrays.asList( //
-					"Mohawk College", //
-					"St Elizabeth Vlg" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("St Elizabeth Vlg", mTrip.getHeadsignId());
-				return true;
-			} else if (Arrays.asList( //
-					"John @ Jackson", //
-					"MacNab Terminal", //
-					"MacNab TT" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("MacNab TT", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 41L) {
-			if (Arrays.asList( //
-					"Mohawk @ Upper James", // <>
-					"Mohawk @ Upper Gage", // <>
-					"41A Chedoke Hosp", //
-					"Mohawk @ Garth", //
-					"Meadowlands" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Meadowlands", mTrip.getHeadsignId());
-				return true;
-			} else if (Arrays.asList( //
-					"Mohawk @ Upper James", // <>
-					"Mohawk @ Upper Gage", // <>
-					"Lime Rdg Mall", //
-					"Gage @ Ind" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Gage @ Ind", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 43L) {
-			if (Arrays.asList( //
-					"Lime Rdg Mall", // <>
-					"Winterberry @ Paramount Only", //
-					"Stone Church @ Upper Paradise", //
-					"Meadowlands" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Meadowlands", mTrip.getHeadsignId());
-				return true;
-			} else if (Arrays.asList( //
-					"Lime Rdg Mall", // <>
-					"Bishop Ryan Secondary School", //
-					"Highland @ Saltfleet HS" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Highland @ Saltfleet HS", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 44L) {
-			if (Arrays.asList( //
-					"Rymal @ Upper James", //
-					"Rymal @ Upper Gage", //
-					"Eastgate Sq", //
-					"Confederation Walmart", //
-					"Confederation " + GO + " Sta" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Confederation " + GO + " Sta", mTrip.getHeadsignId());
-				return true;
-			}
-			if (Arrays.asList( //
-					"Rymal @ Upper James", //
-					"Glancaster Loop", //
-					"Ancaster Business Pk" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Ancaster Business Pk", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 52L) {
-			if (Arrays.asList( //
-					"Ogilvie @ Governors", //
-					"Orchard @ Pleasant" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Orchard @ Pleasant", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 55L) {
-			if (Arrays.asList( //
-					"55A Levi Loop", //
-					"Jones @ Hwy 8" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Jones @ Hwy 8", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 56L) {
-			if (Arrays.asList( //
-					"Confederation Walmart", //
-					"Lakeland CC" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Lakeland CC", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 4344L) { // STM ST. THOMAS MORE
-			if (Arrays.asList( //
-					"Scenic Loop", //
-					"Upper Paradise @ Mohawk", //
-					"Rymal @ Upper James" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Rymal @ Upper James", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 4342L) { // SHER - SHERWOOD SECONDARY
-			if (Arrays.asList( //
-					"Upper Ottawa @ Fennell", //
-					"Upper Gage & Lincoln Alexander South" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Upper Gage & Lincoln Alexander South", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 4343L) { // JDB - ST. JEAN DE BREBEUF
-			if (Arrays.asList( //
-					"John @ Jackson", //
-					"Upper Gage @ Rymal", //
-					"Rymal @ Upper Gage" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Rymal @ Upper Gage", mTrip.getHeadsignId());
-				return true;
-			}
-		} else if (mTrip.getRouteId() == 4013L
-				|| mTrip.getRouteId() == 4106L) { // ANCASTER FAIR SHUTTLE
-			if (Arrays.asList( //
-					"Meadowlands", //
-					"Downtown" //
-			).containsAll(headsignsValues)) {
-				mTrip.setHeadsignString("Downtown", mTrip.getHeadsignId());
-				return true;
-			}
-		}
 		throw new MTLog.Fatal("Unexpected trips to merge %s & %s", mTrip, mTripToMerge);
 	}
 
+	private static final String BURLINGTON = "Burlington";
 	private static final Pattern BURLINGTON_ = CleanUtils.cleanWords("burlinton");
 	private static final String BURLINGTON_REPLACEMENT = CleanUtils.cleanWordsReplacement(BURLINGTON);
 
@@ -424,12 +180,15 @@ public class HamiltonHSRBusAgencyTools extends DefaultAgencyTools {
 	private static final Pattern COMMUNITY_CENTRE = CleanUtils.cleanWords("community centre", "community center");
 	private static final String COMMUNITY_CENTRE_REPLACEMENT = CleanUtils.cleanWordsReplacement(COMMUNITY_CENTRE_SHORT);
 
+	private static final String HAMILTON_AIRPORT_SHORT = "Airport"; // Hamilton
 	private static final Pattern HAMILTON_AIRPORT = CleanUtils.cleanWords("hamilton airport");
 	private static final String HAMILTON_AIRPORT_REPLACEMENT = CleanUtils.cleanWordsReplacement(HAMILTON_AIRPORT_SHORT);
 
+	private static final String HAMILTON_WATERFRONT_SHORT = "Waterfront"; // Hamilton
 	private static final Pattern HAMILTON_WATERFRONT = CleanUtils.cleanWords("hamilton waterfront");
 	private static final String HAMILTON_WATERFRONT_REPLACEMENT = CleanUtils.cleanWordsReplacement(HAMILTON_WATERFRONT_SHORT);
 
+	private static final String MAC_NAB = "MacNab";
 	private static final Pattern MAC_NAB_LC = CleanUtils.cleanWords("macnab");
 	private static final String MAC_NAB_LC_REPLACEMENT = CleanUtils.cleanWordsReplacement(MAC_NAB);
 
@@ -449,18 +208,23 @@ public class HamiltonHSRBusAgencyTools extends DefaultAgencyTools {
 	private static final Pattern TRANSIT_CENTRE = CleanUtils.cleanWords("transit centre", "transit center");
 	private static final String TRANSIT_CENTRE_REPLACEMENT = CleanUtils.cleanWordsReplacement(TRANSIT_CENTRE_SHORT);
 
+	private static final String GO = "GO";
 	private static final Pattern GO_ = CleanUtils.cleanWords("go");
 	private static final String GO_REPLACEMENT = CleanUtils.cleanWordsReplacement(GO);
 
+	private static final String TRANSIT_TERMINAL_SHORT = "TT"; // Transit Terminal
 	private static final Pattern TRANSIT_TERMINAL = CleanUtils.cleanWords("transit terminal");
 	private static final String TRANSIT_TERMINAL_REPLACEMENT = CleanUtils.cleanWordsReplacement(TRANSIT_TERMINAL_SHORT);
 
+	private static final String HIGH_SCHOOL_SHORT = "HS"; // High School
 	private static final Pattern HIGH_SCHOOL_ = CleanUtils.cleanWords("high school");
 	private static final String HIGH_SCHOOL_REPLACEMENT = CleanUtils.cleanWordsReplacement(HIGH_SCHOOL_SHORT);
 
+	private static final String GREENE_SHORT = "Grn";
 	private static final Pattern GREENE_ = CleanUtils.cleanWords("greene");
 	private static final String GREENE_REPLACEMENT = CleanUtils.cleanWordsReplacement(GREENE_SHORT);
 
+	private static final String HERITAGE = "Heritage";
 	private static final Pattern HERITAGE_ = CleanUtils.cleanWords("Heratige");
 	private static final String HERITAGE_REPLACEMENT = CleanUtils.cleanWordsReplacement(HERITAGE);
 
